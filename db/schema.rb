3f8206bae270f82ba7af15a754a885733303d14b
Marcel Degas <marcel.degas@gmail.com>
2015-11-24 20:09:27 -0600

    got the seed data up, madafaka

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125012527) do
+ActiveRecord::Schema.define(version: 20151125015906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20151125012527) do
     t.integer  "students_id"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.string   "name"
   end
 
   add_index "homeworks", ["students_id"], name: "index_homeworks_on_students_id", using: :btree
@@ -64,8 +65,9 @@ ActiveRecord::Schema.define(version: 20151125012527) do
   create_table "teachers", force: :cascade do |t|
     t.string   "name"
     t.integer  "cohort_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "cohort_name"
   end
 
   add_index "teachers", ["cohort_id"], name: "index_teachers_on_cohort_id", using: :btree
