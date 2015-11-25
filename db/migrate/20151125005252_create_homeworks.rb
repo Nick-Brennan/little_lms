bcd4e93bfc96817b6a508fd1a5dbefdd80df80e2
Marcel Degas <marcel.degas@gmail.com>
2015-11-24 18:54:16 -0600

    created models for homework and student

---
+++ db/migrate/20151125005252_create_homeworks.rb
@@ -0,0 +1,12 @@
+class CreateHomeworks < ActiveRecord::Migration
+  def change
+    create_table :homeworks do |t|
+      t.string :hw_submission
+      t.string :skill
+      t.string :student_self_assessment
+      t.integer :student_id
+
+      t.timestamps null: false
+    end
+  end
+end
