6d72bc645acd1e3a3c2acd0c8b970f4fcb5a867e
Humberto Calero <altairn5@uga.edu>
2015-12-03 19:40:31 -0800

    _nav bar links sort of ready

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,14 @@ module ApplicationHelper
   end 
 
   def action?(*action)
-    action.include?(params[:action])
+    @current_action = action.include?(params[:action])
   end
+
+  def not_current_page
+  	binding.pry
+  	current_page?(params[:action]) != @current_action
+  end
+
 
   # def cohort_current
   # 	@current_cohort = current_cohort
