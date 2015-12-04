b9e807e0da36d4bbca4e0fb92af6b2a79dfc5b3a
Humberto Calero <altairn5@uga.edu>
2015-12-03 20:39:02 -0800

    apllication helper added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,13 +8,8 @@ module ApplicationHelper
     @current_action = action.include?(params[:action])
   end
 
-  def not_current_page
-  	binding.pry
-  	current_page?(params[:action]) != @current_action
+  def current_page
+  	current_page?(params[:action]) 
   end
 
-
-  # def cohort_current
-  # 	@current_cohort = current_cohort
-  # end
 end
