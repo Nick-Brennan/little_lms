6364704202d4335cd9858686608d02c5bb14d054
Nick Brennan <nick_p_b@yahoo.com>
2015-12-19 14:25:29 -0800

    changing form input to subject

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 20151219200527) do
 
   create_table "cohorts", force: :cascade do |t|
     t.string   "name"
-    t.string   "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
