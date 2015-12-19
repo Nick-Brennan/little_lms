f066f86e4463c644cccebf0e90db09f7882761cd
justin almeida <justin.h.almeida@gmail.com>
2015-12-19 14:36:06 -0800

    added cohort stuff

--- db/migrate/20151125011002_create_cohorts.rb
+++ db/migrate/20151125011002_create_cohorts.rb
@@ -2,7 +2,7 @@ class CreateCohorts < ActiveRecord::Migration
   def change
     create_table :cohorts do |t|
       t.string :name
-      t.string :subject
+      t.string :type
 
       t.timestamps null: false
     end
